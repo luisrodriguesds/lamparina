6cb378612130bbff18cc9cfeddfadc53c98289bf
luis <luisitaloar@gmail.com>
2019-04-17 15:57:40 -0300

    Correções da pagina de serviços finalizada. Site pronto para receber wordpress

--- paginas/servicos.php
+++ paginas/servicos.php
@@ -1,23 +1,4 @@
 
-    <!-- BreadCrumb Part Start -->
-    <!-- <section class="breadcrumb-part">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center">
-                    <div class="breadcrumb-title">
-                        <h1>PRACTISE AREA</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="breadcrumb-link">
-                <ul class="flat-list">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="#">PRACTISE AREA</a></li>
-                </ul>
-            </div>
-        </div>
-    </section> -->
-    <!-- BreadCrumb Part End -->
 
     <!-- Prictise area Part start -->
     <section class="section-p" style="padding: 80px 0;">
@@ -31,37 +12,16 @@
                 </div>
             </div>
             <div class="row">
-                <!-- <div class="col-lg-4">
-                    <div class="practice-sidebar">
-                        <div class="wid-heading">
-                            <p>Practice List View</p>
-                        </div> 
-                        <div class="prac-sb-list">
-                            <ul>
-                                <li><a href="#">Admiralty (Maritime) Law</a></li>
-                                <li><a href="#">Bankruptcy Law</a></li>
-                                <li><a href="#">Business (Corporate) Law</a></li>
-                                <li><a href="#">Business Law</a></li>
-                                <li><a href="#">Civil Rights (Maritime) Law</a></li>
-                                <li><a href="#">Criminal Law</a></li>
-                                <li><a href="#">Environmental Law</a></li>
-                                <li><a href="#">Financial Law</a></li>
-                                <li><a href="#">Immigration Law</a></li>
-                            </ul>
-                        </div> 
-
-                    </div>
-                
-                </div> -->
 
                 <div class="col-lg-12">
                     
+                    <!-- Seq 1 -->
                     <!-- Single Practice -->
                     <div class="single-practice">
                         <div class="row">
                             <div class="col-lg-8">
                                 <div class="row">
-                                    <div class="col-lg-3">
+                                    <div class="col-sm-4 col-xd-3">
                                         <div class="pra-img d-flex align-items-center">
                                             <!-- <img src="images/practice/1.jpg" alt=""> -->
                                             <div class="bloco">
@@ -71,7 +31,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class=" col-lg-7">
+                                    <div class=" col-sm-7 col-xd-3">
                                         <div class="pra-cont">
                                             <h3>Admiralty (Maritime) Law</h3>
                                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text standard dummy ever since the when an unknown printer took a galley of type.</p>
@@ -82,21 +42,20 @@
                         </div>
                     </div>
                     
-                    
-                    
+                    <!-- Segundo item imprmir dois, lg screen e sm screen -->
                     <!-- Single Practice -->
-                    <div class="single-practice">
+                    <div class="single-practice bloco-right-lg">
                         <div class="row">
                             <div class="col-lg-4"> </div>
                             <div class="col-lg-8">
                                 <div class="row">
-                                    <div class="col-lg-7">
+                                    <div class="col-sm-7 col-xd-3">
                                         <div class="pra-cont text-right">
                                             <h3>Business (Corporate) Law</h3>
                                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
                                         </div>
                                     </div>
-                                    <div class="col-lg-5">
+                                    <div class="col-sm-4 col-xd-3">
                                         <div class="pra-img d-flex align-items-center">
                                             <!-- <img src="images/practice/2.jpg" alt=""> -->
                                             <div class="bloco">
@@ -110,23 +69,51 @@
                             </div>
                         </div>
                     </div>
-
+                    <!-- Single Practice -->
+                    <div class="single-practice bloco-right-sm">
+                        <div class="row">
+                            <div class="col-lg-4"> </div>
+                            <div class="col-lg-8">
+                                <div class="row">
+                                    <div class="col-sm-4 col-xd-3">
+                                        <div class="pra-img d-flex align-items-center">
+                                            <!-- <img src="images/practice/2.jpg" alt=""> -->
+                                            <div class="bloco">
+                                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="pra-img-overlay">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-7 col-xd-3">
+                                        <div class="pra-cont text-center">
+                                            <h3>Business (Corporate) Law</h3>
+                                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- Seq 2 -->
                     <!-- Single Practice -->
                     <div class="single-practice">
                         <div class="row">
                             <div class="col-lg-8">
                                 <div class="row">
-                                    <div class="col-lg-3">
+                                    <div class="col-sm-4 col-xd-3">
                                         <div class="pra-img d-flex align-items-center">
                                             <!-- <img src="images/practice/1.jpg" alt=""> -->
                                             <div class="bloco">
-                                                <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                                                <i class="fa fa-facebook" aria-hidden="true"></i>
                                             </div>
                                             <div class="pra-img-overlay">
                                             </div>
                                         </div>
                                     </div>
-                                    <div class=" col-lg-7">
+                                    <div class=" col-sm-7 col-xd-3">
                                         <div class="pra-cont">
                                             <h3>Admiralty (Maritime) Law</h3>
                                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text standard dummy ever since the when an unknown printer took a galley of type.</p>
@@ -136,24 +123,25 @@
                             </div>
                         </div>
                     </div>
-
+                    
+                    <!-- Segundo item imprmir dois, lg screen e sm screen -->
                     <!-- Single Practice -->
-                    <div class="single-practice">
+                    <div class="single-practice bloco-right-lg">
                         <div class="row">
                             <div class="col-lg-4"> </div>
                             <div class="col-lg-8">
                                 <div class="row">
-                                    <div class="col-lg-7">
+                                    <div class="col-sm-7 col-xd-3">
                                         <div class="pra-cont text-right">
                                             <h3>Business (Corporate) Law</h3>
                                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
                                         </div>
                                     </div>
-                                    <div class="col-lg-5">
+                                    <div class="col-sm-4 col-xd-3">
                                         <div class="pra-img d-flex align-items-center">
                                             <!-- <img src="images/practice/2.jpg" alt=""> -->
                                             <div class="bloco">
-                                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                                                <i class="fa fa-whatsapp" aria-hidden="true"></i>
                                             </div>
                                             <div class="pra-img-overlay">
                                             </div>
@@ -163,99 +151,34 @@
                             </div>
                         </div>
                     </div>
-                        
                     <!-- Single Practice -->
-                    <!-- <div class="single-practice">
+                    <div class="single-practice bloco-right-sm">
                         <div class="row">
-                            <div class="col-lg-5">
-                                <div class="pra-img d-flex align-items-center">
-                                    <img src="images/practice/3.jpg" alt="">
-                                    <div class="pra-img-overlay">
+                            <div class="col-lg-4"> </div>
+                            <div class="col-lg-8">
+                                <div class="row">
+                                    <div class="col-sm-4 col-xd-3">
+                                        <div class="pra-img d-flex align-items-center">
+                                            <!-- <img src="images/practice/2.jpg" alt=""> -->
+                                            <div class="bloco">
+                                                <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="pra-img-overlay">
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="  col-lg-7">
-                                <div class="pra-cont">
-                                    <h3>Experienced Attorneys Maritime</h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
+                                    <div class="col-sm-7 col-xd-3">
+                                        <div class="pra-cont text-center">
+                                            <h3>Business (Corporate) Law</h3>
+                                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
+                                        </div>
+                                    </div>
                                     
                                 </div>
                             </div>
                         </div>
-                    </div> -->
-                    <!-- Single Practice -->
-                    <!-- <div class="single-practice">
-                        <div class="row">
-                            <div class="col-lg-5">
-                                <div class="pra-img d-flex align-items-center">
-                                    <img src="images/practice/4.jpg" alt="">
-                                    <div class="pra-img-overlay">
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="  col-lg-7">
-                                <div class="pra-cont">
-                                    <h3>Deals With Family & Relationship</h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-                    <!-- Single Practice -->
-                    <!-- <div class="single-practice">
-                        <div class="row">
-                            <div class="col-lg-5">
-                                <div class="pra-img d-flex align-items-center">
-                                    <img src="images/practice/5.jpg" alt="">
-                                    <div class="pra-img-overlay">
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="  col-lg-7">
-                                <div class="pra-cont">
-                                    <h3>Fight Donec vitae sapien For Justice</h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-                    <!-- Single Practice -->
-                    <!-- <div class="single-practice">
-                        <div class="row">
-                            <div class="col-lg-5">
-                                <div class="pra-img d-flex align-items-center">
-                                    <img src="images/practice/6.jpg" alt="">
-                                    <div class="pra-img-overlay">
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="  col-lg-7">
-                                <div class="pra-cont">
-                                    <h3>Governments prohibit, except licence.</h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy standard dummy text ever since the when an unknown printer took a galley of type.</p>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
+                    </div>
 
-                    <!-- Practice Pagination-->
-                    <!-- <div class="pra-pagination">
-                        <ul>
-                            <li><a href="#">Previous</a></li>
-                            <li><a href="#">1</a></li>
-                            <li class="active"><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">...</a></li>
-                            <li><a href="#">Next</a></li>
-                        </ul>
-                    </div> -->
-                    
                 </div>
             </div>
         </div>
